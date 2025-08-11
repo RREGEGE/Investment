ae593a1390f92bbe6ae050fcbd0e48107c72ff1e
RREGEGE <ssb10346@naver.com>
2025-08-11 21:17:35 +0900

    사이드 프로젝트 시작

---
+++ Investment/PortpolioManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Investment
+{
+    public class PortfolioManager
+    {
+        private List<Investment> portfolio;
+        
+        private const string filePath = "portfolio.json";
+
+        float investmentAmount = 0;
+        float currentValue = 0;
+        float profitRate = 0;
+
+        public PortfolioManager()
+        {
+            portfolio = LoadPortfolio();
+            InitPortfolio();
+        }
+
+        private List<Investment> LoadPortfolio()
+        {
+            if (File.Exists(filePath))
+            {
+                var jsonString = File.ReadAllText(filePath);
+                return JsonSerializer.Deserialize<List<Investment>>(jsonString) ?? new List<Investment>();
+            }
+            return new List<Investment>();
+        }
+
+        private void SavePortfolio()
+        {
+            var jsonString = JsonSerializer.Serialize(portfolio, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        // 매수 금액, 평가 금액, 이익률 초기화
+        private void InitPortfolio()
+        {
+            float _investmentAmount = 0;
+            float _currentValue = 0;
+
+            foreach(var _investment in portfolio)
+            {
+                _investmentAmount += _investment.purchasePrice;
+                _currentValue += _investment.currentPrice;
+            }
+
+            investmentAmount = _investmentAmount;
+            currentValue = _currentValue;
+            profitRate = currentValue / investmentAmount;
+        }
+
+        // 종목 추가
+        public void AddInvestment(Investment investment)
+        {
+            portfolio.Add(investment);
+            SavePortfolio();
+            InitPortfolio();
+        }
+
+        // 종목 매도 (수량 감소)
+        public void SellInvestment(string name, float quantityToSell)
+        {
+            var investment = portfolio.FirstOrDefault(i => i.name.Equals(name, StringComparison.OrdinalIgnoreCase));
+            if (investment != null)
+            {
+                if (investment.quantity > quantityToSell)
+                {
+                    investment.quantity -= quantityToSell;
+                    investment.Init();
+                }
+                else
+                {
+                    portfolio.Remove(investment);
+                }
+                SavePortfolio();
+                InitPortfolio();
+            }
+        }
+
+        // 포트폴리오 조회
+        public Dictionary<InvestmentType, List<Investment>> GetGroupedPortfolio()
+        {
+            return portfolio.GroupBy(i => i.type)
+                            .ToDictionary(g => g.Key, g => g.ToList());
+        }
+    }
+}
